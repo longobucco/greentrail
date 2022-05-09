3c08ec2aedde1b38737a37b93f5a07e4c70915a7
i4c0ni99 <64600662+i4c0ni99@users.noreply.github.com>
2022-05-09 12:04:04 +0200

    commit creazione progetto con page di home e splash

---
+++ GreenTrail/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouteReuseStrategy} from '@angular/router';
+
+import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
+
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {TranslateModule} from '@ngx-translate/core';
+import {IonicStorageModule} from '@ionic/storage-angular';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,TranslateModule.forRoot(),IonicStorageModule.forRoot()],
+  providers: [{provide: RouteReuseStrategy, useClass: IonicRouteStrategy}],
+  bootstrap: [AppComponent],
+
+})
+
+
+
+
+
+
+
+export class AppModule {
+}
